d42645437c7eb05864a5ff019eee412ef3072f54
ishan neema <ishanneema@gmail.com>
2019-09-03 15:50:27 -0500

    HW 1

---
+++ ISAM5430_homework01/ISAM5430_homework01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISAM5430_homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number 1");
+            string input = Console.ReadLine(); //returns a string
+            int num1 = int.Parse(input);
+            Console.WriteLine("Enter number 2");
+            input = Console.ReadLine(); //reads a line from the keyboard and assigns to the input variable
+            int num2 = Convert.ToInt32(input);
+            Console.WriteLine("Enter the operator: (+,-,/,*)");
+            string op = Console.ReadLine();
+
+            if(op == "+")
+            {
+                int sum;
+                sum = num1 + num2;
+                Console.WriteLine(sum);
+            }
+            if(op == "-")
+            {
+                int sub;
+                sub = num1 - num2;
+                Console.WriteLine(sub);
+            }
+           if(op == "/")
+            {
+                int div;
+                div = num1 + num2;
+                Console.WriteLine(div);
+            }
+           if(op == "*")
+            {
+                int mult;
+                mult = num1 + num2;
+                Console.WriteLine(mult);
+            }
+
+        }
+    }
+}
